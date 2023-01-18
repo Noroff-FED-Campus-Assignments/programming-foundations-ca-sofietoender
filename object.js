fe410e437b84816173d70d8d223761da3bdb29a2
Sofie Tønder <105886175+sofietoender@users.noreply.github.com>
2023-01-18 20:33:18 +0100

    changes

---
+++ object.js
@@ -0,0 +1,13 @@
+const mio = {
+    animal: 'dog',
+    nrOfLegs: 4,
+    isAlive: true,
+    tricks: ['Give Paw', 'Roll over', 'Play Dead'],
+    dogYears: 10,
+    ignition: function() {
+        motorbike.on = !motorbike.on
+           
+} 
+
+}
+
