4580271223401af9d81ff0c18871f6199dd41535
Sofie Tønder <105886175+sofietoender@users.noreply.github.com>
2023-01-19 13:31:33 +0100

    changes

--- object.js
+++ object.js
@@ -1,13 +1,32 @@
-const mio = {
+const myDog = {
+    name: 'Mio',
     animal: 'dog',
     nrOfLegs: 4,
     isAlive: true,
     tricks: ['Give Paw', 'Roll over', 'Play Dead'],
     dogYears: 10,
-    ignition: function() {
-        motorbike.on = !motorbike.on
-           
-} 
+    humanYears: function (dogYears, humanYearsFactor = 7) {
+        return dogYears * humanYearsFactor;
+    }
+    
 
 }
+function changeImage() {
+    let image = document.getElementById("dogImage");
+    if (image.src.match("mio1.jpg")) {
+      image.src = "mio2.jpg";
+    } else {
+      image.src = "mio1.jpg";
+    }
+  }
+  const ageParagraph = document.getElementById("infoAboutMio");
+  ageParagraph.innerHTML = `My dog's name is ${myDog.name}, he is a ${myDog.animal} with ${myDog.nrOfLegs} legs and currently ${myDog.isAlive ? 'alive' : 'not alive'}. He knows how to do tricks like ${myDog.tricks.join(', ')}. He is currently ${myDog.dogYears} years old in dog years, which is equivalent to ${myDog.humanYears(myDog.dogYears)} human years.`
+
+  
+  
+
+
+
+
+    
 
